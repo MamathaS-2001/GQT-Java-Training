021cf58b9da0bc1d51b8dee49167cbc35734d0fc
mamatha s <mamatha s@DESKTOP-JPSK0CO>
2023-03-23 07:06:20 +0530

    SecondCommit

--- src/GqtCodes/Program.java
+++ src/GqtCodes/Program.java
@@ -5,6 +5,9 @@ public class Program {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		System.out.println("Welcome to GQT Sessions");
+		
+		
 	}
 
 }
